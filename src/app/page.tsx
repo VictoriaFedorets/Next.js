36a4d5814bbc1ffe5ad12494db1c888d26ede199
Victoria <vika96karnauhova@gmail.com>
2025-03-18 23:48:18 +0200

    fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import AddCompanyButton from '@/app/components/add-company-button';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
-      <AddCompanyButton />
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-3xl">Welcome to the TruScape Dashboard!</h1>
+        <p className="text-2xl mb-5">Manage all your companies and keep track of important information in one place.</p>
+        <AddCompanyButton />
+      </div>
     </main>
   );
 }
